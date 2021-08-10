9dcf9bcd2c1f3b23d13825dea24e9d42c539d383
EvGRyder <evryder92@gmail.com>
2021-08-10 16:08:25 -0300

    made buttons smaller so its easier on mobile

--- src/tabs.js
+++ src/tabs.js
@@ -1,6 +1,6 @@
 function navButtonGen(namen) {
     const newButton = document.createElement('button')
-    newButton.style.padding = "10px 35px 10px 35px"
+    newButton.style.padding = "10px 15px 10px 15px"
     newButton.style.backgroundColor = "black"
     newButton.style.color = "white"
     newButton.style.borderRadius = "60px"
